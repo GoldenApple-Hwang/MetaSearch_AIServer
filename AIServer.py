ede790646d383ee2f2cc7eadae0a6d765cc72e34
YeWon Seo <syw1236@naver.com>
2024-04-25 15:38:29 +0900

    feat: neo4j 서버에 csv 파일 요청 구현
    neo4j 서버에 csv 파일을 요청 후 해당 파일 저장

--- AIServer.py
+++ AIServer.py
@@ -60,13 +60,30 @@ def upload_image():
     except Exception as e:
            print(f'폴더 생성 중 오류가 발생함 : {str(e)}')
 
+    #csv 파일 저장 경로, 파일 이름
     csv_file_path = os.path.join(csv_directory, FOLDER_NAME+".csv")
 
-    if not os.path.exists(csv_file_path):
-         with lock:  # Lock을 획득하여 해당 블록을 동기화
+   #request_csv_neo4jServer(FOLDER_NAME) # neo4j 서버에 csv 파일 요청
+
+    # # 현재 csv_file_path에 해당 csv 파일이 존재하지 않는 경우,
+    # if not os.path.exists(csv_file_path):
+    #      with lock:  # Lock을 획득하여 해당 블록을 동기화
+    #         with open(csv_file_path, 'w', newline='') as csv_file:  # 새 파일 생성
+    #             csv_writer = csv.writer(csv_file)
+    #             csv_writer.writerow(["Entity1", "Relationship", "Entity2"])  # 헤더 추가
+    # else: 
+    #     # csv_file_path에 해당 csv 파일이 존재하는 경우
+
+    # neo4j 서버에 csv 파일 요청 및 저장    
+    isCSVFile = request_csv_neo4jServer(FOLDER_NAME,csv_file_path) 
+
+    if not isCSVFile: #neo4j 서버에서 csv 파일을 가져오지 못한다면 
+        with lock:  # Lock을 획득하여 해당 블록을 동기화
             with open(csv_file_path, 'w', newline='') as csv_file:  # 새 파일 생성
                 csv_writer = csv.writer(csv_file)
                 csv_writer.writerow(["Entity1", "Relationship", "Entity2"])  # 헤더 추가
+
+
 
     if 'addImage' in request.files: # 추가된 이미지에 대한 분석 요청
         #print('image로 뽑힌 것 ')
@@ -187,13 +204,41 @@ def delete_csv_file_info(csv_file_path,filename):
         csv_dataframe.to_csv(csv_file_path,index=False)
 
 
+#neo4j 서버에 csv 요청
+def request_csv_neo4jServer(database,csv_file_path):
+    # 저장되어야하는 csv 파일 경로
+    #csv_path = f'./{database}/CSV/' 
+
+    #neo4j 서버의 url
+    neo4j_url = f'http://113.198.85.4/neo4jserver/csv'
+
+    # 전송할 데이터베이스 이름
+    database_name = {'dbName':database}
+
+    #JSON 형태로 데이터베이스 이름을 POST 방식으로 전송
+    response = requests.post(neo4j_url,json=database_name)
+
+    #서버 응답 확인
+    if response.status_code == 200:
+        #서버로부터 받은 CSV 파일 저장
+        with open(csv_file_path, 'wb') as file:
+            file.write(response.content)
+        print('Successfully saved the CSV file.')
+        return True
+    else:
+        print('Failed to receive the CSV file.')
+        return False
+
+
+
+
+
 # neo4j 서버에 csv 전송
 def send_neo4jServer(csv_file_path):
-    web_url = 'http://113.198.85.4/aiserver/uploadcsv'  # Node.js 서버의 엔드포인트
+    neo4j_url = 'http://113.198.85.4/aiserver/uploadcsv'  # Node.js 서버의 엔드포인트
     files = {'csvfile': open(csv_file_path, 'rb')}  # 'example.csv'는 전송하고자 하는 파일명
-    response = requests.post(web_url, files=files) # node.js에 파일 전송
+    response = requests.post(neo4j_url, files=files) # node.js에 파일 전송
     print(response.text)  # 서버의 응답 출력
-
 
 
 # 데이터베이스 이미지 요청
