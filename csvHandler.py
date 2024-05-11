42948ed95f0d5ec34be8af1880302904c0bc408d
YeWon Seo <syw1236@naver.com>
2024-05-11 20:15:31 +0900

    refactor: csv 파일 수정 후 닫기 기능 수정
    csv 파일 수정한 후에 with open으로 바로 디스크에 적용되도록 수정함

--- csvHandler.py
+++ csvHandler.py
@@ -4,20 +4,25 @@ import pandas as pd
 import os
 
 # csv 파일에 파일이름 들어간 행 삭제하는 함수
-def delete_csv_file_info(csv_file_path,filename):
-    #csv 파일을 dataframe으로 불러옴
+def delete_csv_file_info(csv_file_path, filename):
+    print("delete_csv_file_iffo 함수 호출함")
+    # csv 파일을 dataframe으로 불러옴
     csv_dataframe = pd.read_csv(csv_file_path)
+    
+    print("현재 삭제하려는 filename: "+filename)
 
-    #'Entity1' 컬럼에서 filename과 같은 값을 가진 행을 모두 삭제
+    # 'Entity1' 컬럼에서 filename과 같은 값을 가진 행을 모두 삭제
     csv_dataframe = csv_dataframe[csv_dataframe['Entity1'] != filename]
 
-    #변경된 DataFrame을 다시 CSV 파일로 저장
-    csv_dataframe.to_csv(csv_file_path,index=False)
+    # 변경된 DataFrame을 다시 CSV 파일로 저장하고 파일을 닫음
+    with open(csv_file_path, 'w', encoding='utf-8') as f:
+        csv_dataframe.to_csv(f, index=False)
 
 
 # CSV 파일에서 특정 조건을 만족하는 행을 삭제하기 전에 추가 조건 확인하고 해당 값을 반환하는 함수
 # 인물 속성이 존재하면 해당 속성값을 반환한다.
 def return_entity2_if_relationship_is_person(csv_file_path, filename):
+    print("return_entity2_if_relationship_is_person 함수 호출")
     # CSV 파일을 DataFrame으로 로드
     csv_dataframe = pd.read_csv(csv_file_path)
     
@@ -26,6 +31,9 @@ def return_entity2_if_relationship_is_person(csv_file_path, filename):
     
     # 조건을 만족하는 'Entity2'의 값들을 리스트로 저장
     entity2_values_if_person = csv_dataframe[condition]['Entity2'].tolist()
+
+    # csv 파일 삭제함 (filename이 행만)
+    delete_csv_file_info(csv_file_path,filename)
     
     # 조건을 만족하는 'Entity2'의 값을 반환
     return entity2_values_if_person
